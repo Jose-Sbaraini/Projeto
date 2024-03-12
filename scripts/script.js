98c88579a1a2f23ceb8d002e513e0a6b5353408d
José <josesbarainips@gmail.com>
2024-03-12 17:41:45 -0400

    Att

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,5 @@
 document.getElementById("amongus-image").addEventListener("click", function() {
     document.getElementById("amongus-image").style.display = "none";
+    document.getElementById("crewin").style.display = "block";
+    document.getElementById("sus-2").style.textalign = "center";
   });
