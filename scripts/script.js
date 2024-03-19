7cabc347ba6ec1bc55bc449c42c4ceed5c0f039c
José <josesbarainips@gmail.com>
2024-03-19 17:39:32 -0400

    easteregg

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,33 @@
-document.getElementById("amongus-image").addEventListener("click", function() {
-    document.getElementById("cba").style.display = "none";
-    document.getElementById("abc").style.display = "block";
-  });
 
-  document.getElementById("crewin").addEventListener("click", function() {
-    document.getElementById("abc").style.display = "none";
-    document.getElementById("cba").style.display = "block";
-  });
+var impost =(Math.random())*100
+
+
+
+
+
+
+
+document.getElementById("amongus-image").addEventListener("click", function() {
+  if (impost>95) {document.getElementById("amongus-image").style.display = "none";
+    document.getElementById("crewin").style.display = "block";
+    
+  }
+  else if (impost<=95) {document.getElementById("amongus-image").style.display = "none";
+  document.getElementById("imp").style.display = "block";
+
+
+
+  }
+
+    
+  })
+
+
+
+
+ 
+document.getElementById("imp").addEventListener("click", function() {
+  document.getElementById("amongus-image").style.display = "block";
+  document.getElementById("imp").style.display = "none";
+
+});
