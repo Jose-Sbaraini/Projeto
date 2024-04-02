087e353142598d8344a344ec14b75e746c5051d6
josesbaraini <josesbarainips@gmail.com>
2024-04-02 17:21:09 -0400

    add impostor

--- scripts/script.js
+++ scripts/script.js
@@ -9,12 +9,18 @@ let impost =(Math.random())*100
 
 
 document.getElementById("amongus-image").addEventListener("click", function() {
-  if (impost>95) {document.getElementById("amongus-image").style.display = "none";
+  if (impost>95) {
+    document.getElementById("amongus-image").style.display = "none";
     document.getElementById("crewin").style.display = "block";
     
   }
-  else if (impost<=95) {document.getElementById("amongus-image").style.display = "none";
+  else if (impost<=95){
+  document.getElementById("amongus-image").style.display = "none";
   document.getElementById("imp").style.display = "block";
+
+  document.getElementById("impostor").style.display = "block";
+  document.getElementById("base").style.display = "none";
+  
   impost =(Math.random())*100
 
 
@@ -31,5 +37,9 @@ document.getElementById("amongus-image").addEventListener("click", function() {
 document.getElementById("imp").addEventListener("click", function() {
   document.getElementById("amongus-image").style.display = "block";
   document.getElementById("imp").style.display = "none";
+
+  
+  document.getElementById("base").style.display = "block";
+  document.getElementById("impostor").style.display = "none";
 
 });
